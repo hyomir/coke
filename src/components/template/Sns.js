4fc9e7997618a5a07e0297cded2d7416b73915ac
hyorim <hyorimseo9808@gmail.com>
2024-09-09 10:03:14 +0900

    컴포넌트 수정

--- src/components/template/Sns.js
+++ src/components/template/Sns.js
@@ -5,7 +5,15 @@ function SNS() {
     return (
         <div className="sns-section">
             <div className="sns-wrap">
-                <h2>SNS</h2>
+                <div className="sns_text">
+                    <p>다양한 곳에서 코카-콜라를 만나보세요.</p>
+                    <ul>
+                        <li><a href="#none">유투브</a></li>
+                        <li><a href="#none">트위터</a></li>
+                        <li><a href="#none">페이스북</a></li>
+                        <li><a href="#none">인스타그램</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
