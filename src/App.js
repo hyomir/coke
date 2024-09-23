f895fc6b314dc1726a2db670834008ac5c80ed43
hyorim <hyorimseo9808@gmail.com>
2024-09-24 06:55:08 +0900

    git page

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
 import { useEffect } from 'react';
-import { createBrowserRouter,createRoutesFromElements, Route, RouterProvider,BrowserRouter } from 'react-router-dom';
-import Lenis from '@studio-freight/lenis'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, BrowserRouter } from 'react-router-dom';
+import Lenis from '@studio-freight/lenis';
 import './App.css';
-import RootLayout from './components/RootLayout'
-import Home from './components/home/Home'
-import Brand from './components/brand/Brand'
-import Detail from 'components/brand/Detail'
-import Company from './components/history/Company'
-import Since from './components/history/Since'
-import Story from './components/story/Story'
-import SocialValue from './components/socialValue/SocialValue'
-import Sustain from './components/socialValue/Sustain'
+import RootLayout from './components/RootLayout';
+import Home from './components/home/Home';
+import Brand from './components/brand/Brand';
+import Detail from './components/brand/Detail';
+import Company from './components/history/Company';
+import Since from './components/history/Since';
+import Story from './components/story/Story';
+import SocialValue from './components/socialValue/SocialValue';
+import Sustain from './components/socialValue/Sustain';
 import Notfound from './components/main/Notfound';
 import SearchBox from './components/main/SearchBox';
-import SearchResults from './components/main/SearchResults'
+import SearchResults from './components/main/SearchResults';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element ={<RootLayout/>}>
-      <Route index element={<Home/>}></Route>
+    <Route path='/' element={<RootLayout />}>
+      <Route index element={<Home />} />
       <Route path="/search" element={<SearchBox />} />
-        <Route path="/result/:id" element={<SearchResults/>} />
-      <Route path ="brand" element={<Brand/>}>
-      <Route path="detail/:id" element={<Detail/>}></Route>
+      <Route path="/result/:id" element={<SearchResults />} />
+      <Route path="brand" element={<Brand />}>
+        <Route path="detail/:id" element={<Detail />} />
       </Route>
-      <Route path="company" element={<Since/>}></Route>
-      <Route path="since" element={<Since/>}></Route>
-      <Route path ="story" element={<Story/>}></Route>
-      <Route path ="social" element={<Sustain/>}></Route>
-      <Route path="sustainability" element={<Sustain/>}></Route>
-      <Route path="*" element={<Notfound/>}></Route>
+      <Route path="company" element={<Since />} />
+      <Route path="since" element={<Since />} />
+      <Route path="story" element={<Story />} />
+      <Route path="social" element={<Sustain />} />
+      <Route path="sustainability" element={<Sustain />} />
+      <Route path="*" element={<Notfound />} />
     </Route>
   )
-)
+);
 
-function App(){
+function App() {
   useEffect(() => {
     const lenis = new Lenis();
-        lenis.on('scroll', (e) => {
+    lenis.on('scroll', (e) => {
       console.log(e);
     });
     const raf = (time) => {
@@ -49,6 +49,7 @@ function App(){
       lenis.destroy();
     };
   }, []);
+
   return (
     <div className='App'>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
